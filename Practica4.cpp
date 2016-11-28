4456569c6766fc681a60673c8f62e377d4fe91f1
ho-la <qaz111wsx222@gmail.com>
2016-11-28 16:19:37 +0100

    Creado

---
+++ Practica4.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: odopaon
+ *
+ * Created on 28 de noviembre de 2016, 16:16
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
